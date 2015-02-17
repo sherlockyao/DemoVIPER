34d29503ac5f91b2ff4a6be8a9857c8494ab66e5
Sherlock Yao <yaosherlock@gmail.com>
2015-02-17 12:03:25 +0800

    add layout for 'Menu Panel' view

---
+++ DemoVIPER/Classes/Views/MenuPanelView.h
@@ -0,0 +1,15 @@
+//
+//  MenuPanelView.h
+//  DemoVIPER
+//
+//  Created by Sherlock on 2/17/15.
+//  Copyright (c) 2015 originate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuPanelView : UIView
+
+@property (weak, nonatomic) IBOutlet UITableView *themeTableView;
+
+@end
---
+++ DemoVIPER/Classes/Views/MenuPanelView.m
@@ -0,0 +1,21 @@
+//
+//  MenuPanelView.m
+//  DemoVIPER
+//
+//  Created by Sherlock on 2/17/15.
+//  Copyright (c) 2015 originate. All rights reserved.
+//
+
+#import "MenuPanelView.h"
+
+@implementation MenuPanelView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
