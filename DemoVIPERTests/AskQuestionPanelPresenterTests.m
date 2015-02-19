c3b320f19ac6dcdd3fb1c5f10af3ea25170bfcf8
Sherlock Yao <yaosherlock@gmail.com>
2015-02-19 16:54:16 +0800

    add tests for presenters

---
+++ DemoVIPERTests/AskQuestionPanelPresenterTests.m
@@ -0,0 +1,76 @@
+//
+//  AskQuestionPanelPresenterTests.m
+//  DemoVIPER
+//
+//  Created by Sherlock on 2/19/15.
+//  Copyright (c) 2015 originate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+#import <OCMock/OCMock.h>
+#import "AskQuestionPanelPresenter.h"
+
+@interface AskQuestionPanelPresenterTests : XCTestCase
+
+@property (nonatomic, strong) AskQuestionPanelPresenter *presenter;
+@property (nonatomic, strong) id progresshudMock;
+@property (nonatomic, strong) id quesionListMock;
+@property (nonatomic, strong) id answerBoardMock;
+
+@end
+
+@implementation AskQuestionPanelPresenterTests
+
+- (void)setUp {
+  [super setUp];
+  self.presenter = [AskQuestionPanelPresenter new];
+  self.presenter.questionRobotInteractor = OCMClassMock([QuestionRobotInteractor class]);
+
+  self.progresshudMock = OCMProtocolMock(@protocol(ProgressHudInterface));
+  self.quesionListMock = OCMProtocolMock(@protocol(QuestionListInterface));
+  self.answerBoardMock = OCMProtocolMock(@protocol(AnswerBoardInterface));
+  self.presenter.progresshud = self.progresshudMock;
+  self.presenter.questionList = self.quesionListMock;
+  self.presenter.answerBoard = self.answerBoardMock;
+}
+
+- (void)tearDown {
+  self.presenter = nil;
+  self.progresshudMock = nil;
+  self.quesionListMock = nil;
+  [super tearDown];
+}
+
+- (void)testEnteringQuestionText {
+  self.presenter.candidateQuestionCount = 7;
+  NSArray *questons = @[@"1", @"2"];
+  id questionRobotInteractorMock = self.presenter.questionRobotInteractor;
+  [[[questionRobotInteractorMock stub] andReturn:questons] findSimilarQuestionsWithText:@"test" limitedCount:7];
+
+  [[self.quesionListMock expect] displayQuestions:questons];
+  
+  [self.presenter enteringQuestionText:@"test"];
+  
+  [self.quesionListMock verify];
+}
+
+- (void)testAskQuestion {
+  AnswerInfo *answer = [AnswerInfo new];
+  id questionRobotInteractorMock = self.presenter.questionRobotInteractor;
+  [[[questionRobotInteractorMock stub] andDo:^(NSInvocation *invoke) {
+    void (^askQuestoinStubResponse)(AnswerInfo *answer);
+    [invoke getArgument:&askQuestoinStubResponse atIndex:3];
+    askQuestoinStubResponse(answer);
+  }] askQuestion:[OCMArg any] completion:[OCMArg any]];
+  
+  [[self.progresshudMock expect] beginProgress];
+  [[self.answerBoardMock expect] displayAnswer:answer];
+  
+  [self.presenter askQuestion:nil];
+  
+  [self.progresshudMock verify];
+  [self.answerBoardMock verify];
+}
+
+@end
